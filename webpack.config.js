ba97ee3e48f595a9e1cf7d099afc7d74b16b4dd4
knownothingsnow <409268215@qq.com>
2016-02-03 18:07:16 +0800

    add webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ var path = require('path')
 
 module.exports = {
   entry: {
-    app: '../src/main.js'
+    app: './src/main.js'
   },
   output: {
     path: path.resolve(__dirname, '../dist/static'),
