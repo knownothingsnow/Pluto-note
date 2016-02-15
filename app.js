e88a3db9bcc6a55a175fd276313b3a7c8dec8971
Knight Young <409268215@qq.com>
2016-02-16 01:24:28 +0800

    完善目录,改用ejs

--- app.js
+++ app.js
@@ -5,24 +5,23 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
+var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
 
 // view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'jade');
-
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
+app.use('/index', index);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
